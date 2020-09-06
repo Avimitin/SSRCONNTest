44a4a15f2eba17861c0ee5b61de3a58d45aec0d8
Avimitin <avimitin@gmail.com>
2020-09-06 17:02:42 +0800

    删除掉无用依赖

--- configs/db_cfg.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# author: Avimitin
-# datetime: 2020/8/30 16:53
-CONFIG = {
-        "host": "localhost",
-        "user": "root",
-        "password": "MySQL_pwd$0829",
-        "database": "server"
-}
