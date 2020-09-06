a950ae99b3129f43c43f6c6c04b84652e4a78dc0
Avimitin <avimitin@gmail.com>
2020-09-06 16:19:31 +0800

    调用封装的get_cur 方法时会重新生成指针

--- web/bin/database/DBConnector.py
+++ web/bin/database/DBConnector.py
@@ -2,7 +2,7 @@
 # author: Avimitin
 # datetime: 2020/8/30 16:31
 import pymysql
-from config import db_cfg
+from configs import db_cfg
 
 DATABASE_CFG = db_cfg.CONFIG
 
@@ -15,13 +15,12 @@ class DBConnect:
             password=DATABASE_CFG["password"],
             database=DATABASE_CFG["database"]
         )
-        self.cur = self.database.cursor()
 
     def get_db(self):
         return self.database
 
     def get_cur(self):
-        return self.cur
+        return self.database.cursor()
 
     def close_connect(self, curIsOpen=False):
         if curIsOpen:
