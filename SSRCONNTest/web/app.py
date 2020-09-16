bccda9f5fb8a84c36b20ae905ec7d0614ae156a6
Avimitin <avimitin@gmail.com>
2020-09-16 16:49:05 +0800

    简化subscriptions的操作代码；将token验证改为模块

--- SSRCONNTest/web/app.py
+++ SSRCONNTest/web/app.py
@@ -1,13 +1,13 @@
+from web.utils.TokenAuthorize import auth
 from flask import Flask, request, jsonify, make_response
 from web.static import APIRETURN
-from utils.database import ResultHandler, SubHandler, UserHandler
-from utils.TokenGenerator import Generator
+from web.static.APIRETURN import add_more_info as add
+from utils.database import ResultHandler, SubHandler
 import configparser
 from web.utils import Register, TokenAuthorize
+import json
 
 app = Flask(__name__)
-
-__TOKEN__ = "dev"
 
 __VERSION__ = "0.0.1"
 
@@ -19,10 +19,12 @@ def redirect():
 
 @app.route('/api/v1/results', methods=["POST"])
 def task_handler():
-    if request.json is None or "token" not in request.json:
-        return make_response(jsonify(APIRETURN.EMPTY), 200)
+    if not request.form.get("token"):
+        return make_response(APIRETURN.EMPTY, 200)
 
-    if request.json["token"] == __TOKEN__:
+    token = request.form.get("token")
+
+    if TokenAuthorize.auth(token):
         name = request.args.get("name")
         time = request.args.get("time")
         r = ResultHandler.ResultHandler()
@@ -33,85 +35,67 @@ def task_handler():
         elif time:
             result = r.get_result_by_keyword(time=time)
         else:
-            return make_response(jsonify(APIRETURN.EMPTY_ARGS_GUIDE), 400)
-        return make_response(jsonify(result), 200)
+            return make_response(APIRETURN.EMPTY_ARGS_GUIDE, 400)
+        return make_response(result, 200)
     else:
-        return make_response(jsonify(APIRETURN.UNAUTHORIZED), 401)
+        return make_response(APIRETURN.UNAUTHORIZED, 401)
 
 
 @app.route("/api/v1/subscriptions", methods=["POST"])
 def subscriptions_handler():
-    if request.form is None or "token" not in request.form:
+    token = request.form.get("token")
+    if not token:
         return make_response(jsonify(APIRETURN.EMPTY), 200)
 
-    if request.form["token"] == __TOKEN__:
-        try:
-            subs = request.form["subs"]
-            if not subs:
-                return make_response(
-                    APIRETURN.add_more_info(
-                        APIRETURN.EMPTY,
-                        "YOU MUST ADD SOME SUB INFO IN THE LIST"
-                    ),
-                    200
-                )
-
-            for sub in subs:
-                if not isinstance(sub, dict):
-                    return make_response(
-                        APIRETURN.add_more_info(APIRETURN.INVALID, "OBJECT IN LIST MUST BE DICT"), 
-                        400)
-                
-                if "name" not in sub.keys() and "link" not in sub.keys():
-                    return make_response(
-                        APIRETURN.add_more_info(APIRETURN.INVALID, "UNKNOWN KEY, NEED ARGS LIKE THIS: "
-                                                                    "{'name': 'name', 'link': 'link'}"),
-                        400
-                    )
-                if sub["name"] is None or sub["link"] is None:
-                    return make_response(
-                        APIRETURN.add_more_info(APIRETURN.INVALID, "CANT NOT INPUT NULL VALUES"), 400)
-                
-                s = SubHandler.SubHandler()
-                result = s.add_new_subscriptions(sub["name"], sub["link"])
-                
-                if result["ok"]:
-                    return make_response(APIRETURN.add_more_info(APIRETURN.OK, "INSERTION DONE"), 200)
-                
-                return make_response(APIRETURN.INTERNAL_SERVER_ERROR, 500)
-
-        except KeyError:
-            if not request.form.get("search"):
-                return make_response(
-                    jsonify(APIRETURN.add_more_info(APIRETURN.INVALID, "PUT SUBS OR SEARCH INTO YOUR POST FILE")), 400)
-
-            args = request.form.get("search")
-            if not isinstance(args, list):
-                return make_response(
-                        jsonify(APIRETURN.add_more_info(APIRETURN.INVALID, "SEARCH MUST BE A LIST"), 400))
-
-            s = SubHandler.SubHandler()
-            results = []
-            for arg in args:
-                response = s.get_sub_link_by_name(arg)
-                if response:
-                    results.append(response)
-
-            return make_response(jsonify(ok=True, results=results), 200)
-
-        except IndexError:
-            return make_response(
-                    APIRETURN.add_more_info(
-                        APIRETURN.INVALID,
-                        'Exp: '
-                        '{"subs": [{"name": "name", "link": "link"}]}'
-                    ),
-                400
-            )
-        except Exception:
-            return make_response(jsonify(ok=False, descriptions="Unknow Error Occur", more_info=Exception), 500)
-    else:
+    if not TokenAuthorize.auth(token):
         return make_response(jsonify(APIRETURN.UNAUTHORIZED), 401)
+
+    subs = request.form.get("subs")
+
+    if subs:
+        try:
+            subs = json.loads(subs)
+        except json.decoder.JSONDecodeError:
+            return make_response(add(APIRETURN.INVALID, "Expected JSON format style"), 400)
+        s = SubHandler.SubHandler()
+        for sub in subs:
+            if not isinstance(sub, dict):
+                return make_response(add(APIRETURN.INVALID, "OBJECT IN LIST MUST BE DICT"), 400)
+
+            name = sub.get("name")
+            link = sub.get("link")
+
+            if not (name and link):
+                return make_response(add(APIRETURN.INVALID, "CANT NOT INPUT NULL VALUES"), 400)
+
+            result = s.add_new_subscriptions(name, link)
+
+            if not result["ok"]:
+                return make_response(APIRETURN.INTERNAL_SERVER_ERROR, 500)
+        
+        return make_response(add(APIRETURN.OK, "INSERTION DONE"), 200)
+
+    args = request.form.get("search")
+
+    if not args:
+        return make_response(add(APIRETURN.EMPTY, "Server expected subs link or search args but get none"), 400)
+    
+    try:
+        args = json.loads(args)
+    except json.decoder.JSONDecodeError:
+        return make_response(add(APIRETURN.INVALID, "Expected JSON format style"), 400)
+
+    if not isinstance(args, list):
+        return make_response(add(APIRETURN.INVALID, "SEARCH MUST BE A LIST"), 400)
+
+    s = SubHandler.SubHandler()
+    results = []
+    for arg in args:
+        response = s.get_sub_link_by_name(arg)
+        if response:
+            results.append(response)
+
+    return make_response(jsonify(ok=True, results=results), 200)
 
 
 @app.route("/api/v1/verification", methods=["POST"])
@@ -123,27 +107,17 @@ def verify():
         token = request.form.get("token")
         if not token:
             return make_response(
-                jsonify(APIRETURN.add_more_info(APIRETURN.EMPTY, "First time verify need a StartUpToken")),
-                400
-            )
+                add(APIRETURN.EMPTY, "Verification need a token but get none"), 400)
         if token != StartUpToken:
-            return make_response(
-                jsonify(APIRETURN.add_more_info(APIRETURN.UNAUTHORIZED, "Unexpected token")),
-                401)
+            return make_response(add(APIRETURN.UNAUTHORIZED, "Unexpected token"), 401)
 
         name = request.form.get("name")
         uid = request.form.get("uid")
         permission = "admin"
         if not (name and uid):
-            return make_response(
-                jsonify(APIRETURN.add_more_info(APIRETURN.INVALID, "Missing arguments")),
-                400
-            )
-        g = Generator.TokenGenerator()
-        salt, token = g.new(name, uid)
+            return make_response(add(APIRETURN.INVALID, "Missing arguments"), 400)
 
-        u = UserHandler.UserHandler()
-        result = u.add_users(uid, name, permission, token)
+        result = Register.register(name, uid, permission)
 
         if result["ok"]:
             parser.set("Privacy", "StartUpToken", "0")
@@ -151,18 +125,21 @@ def verify():
 
         return make_response(jsonify(result), 200)
 
-    return make_response(APIRETURN.add_more_info(APIRETURN.NOTFOUND, "This page has been removed."), 404)
+    return make_response(add(APIRETURN.NOTFOUND, "This page has been removed."), 404)
 
 
-@app.route("/api/v1/users")
+@app.route("/api/v1/users", methods=["POST"])
 def handle_users():
     return make_response()
+
 
 @app.errorhandler(404)
 def not_fount(error):
     return make_response(jsonify(APIRETURN.NOTFOUND), 404)
 
-
+@app.errorhandler(405)
+def error_method(error):
+    return make_response(jsonify(ok=False, descriptions="This method is not allow"), 405)
 
 if __name__ == '__main__':
     app.run(debug=True)
